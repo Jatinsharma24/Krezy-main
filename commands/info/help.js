b79ec8324def055e9ce87556887413b98b2188af
Glitch (alpine-ionized-handstand) <none>
2021-01-08 14:36:27 +0000

    👼⚽️ Checkpoint ./commands/info/help.js:25550912/132

--- commands/info/help.js
+++ commands/info/help.js
@@ -15,12 +15,13 @@ module.exports = {
     .setColor(Color)
     .setTitle(`${client.user.username} Commands!`)
     .setDescription(`Use ${Prefix}Help <Command Name> For More Command Information!` + 
-    "\n\n**Fun**\n`Avatar, Coinflip, Howgay, Meme, Rate, 8ball, Dicksize, Ascii, Choose, Hack, Randomnumber`" + "\n\n" + "**Moderation**\n`Clear, Mute, Unmute, Tempmute, Kick, Ban, Unban, Tempban, Warn, Warnings, ResetWarns`" + "\n\n"+
-    "**Information**\n`Help, Covid, Weather, Userinfo, Serverinfo, Ping`"
+    "\n\n**Fun**\n`Avatar, Coinflip, Howgay, Meme, Rate, 8ball, Dicksize, Ascii, Choose, Hack, Randomnumber`"
+     + "\n\n"+"**Moderation**\n`Clear, Mute, Unmute, Tempmute, Kick, Ban, Unban, Tempban, Warn, Warnings, ResetWarns`" 
+     + "\n\n"+"**Information**\n`Help, Covid, Weather, Userinfo, Serverinfo, Ping`"
      + "\n\n"+"**Music Commands** \n`M$help for Music Commands`"
      + "\n\n"+"**Giveaway Commands** \n`G$help for Giveaway Commands`"
      + "\n\n"+"**Economy Commands** \n`E$help for Economy Commands`"
-     + "\n\n"+"**Welcome Setup** \n`wel$help for Welcome Setup (5 Minutes)`"
+     + "\n\n"+"**Welcome Setup** \n`wel$help for Welcome Setup`"
      + "\n\n"+"**Bot Owner** \n`Cóöl Düdé <⁶⁹>#5158`")
     .setFooter(`Requested By ${message.author.username}`)
     .setTimestamp();
