6a2d8d6e5383e07adfb60dbf08eb3851dd441c58
Glitch (alpine-ionized-handstand) <none>
2021-01-10 11:02:55 +0000

    👛🍴 Checkpoint ./commands/info/help.js:26374886/4547

--- commands/info/help.js
+++ commands/info/help.js
@@ -29,11 +29,9 @@ module.exports = {
           "\n\n" +
           "**Reaction roles** \n`r$help for reaction roles commands`" +
           "\n\n" +
-          "**Entrance/Exit Setup.(beta)** \n`W$help for welcome setup!, Leave$help for exit setup!`"
+          "**Entrance/Exit Setup.** \n`W$help for welcome setup!, Leave$help for exit setup!`"
       )
-      .setFooter(
-        `To get info of each command you can do $help [command] | Handle by Cööl dude 69`
-      )
+      .setFooter(`Handle by Cööl dude 69`)
       .setTimestamp();
 
     if (!args.length) return message.channel.send(embed);
